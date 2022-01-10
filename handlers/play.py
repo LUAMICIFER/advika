87914f17664fbfcd5124733335fe947abae629fb
LUAMICIFER <96227537+LUAMICIFER@users.noreply.github.com>
2022-01-10 19:58:54 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -367,7 +367,7 @@ async def play(_, message: Message):
         await message.reply_photo(
             photo="final.png",
             reply_markup=keyboard,
-            caption="**CHALIYE SHURU KARTE HAI.format(
+            caption="**CHALIYE SHURU KARTE HAI**".format(
         message.chat.title
         ), )
 
